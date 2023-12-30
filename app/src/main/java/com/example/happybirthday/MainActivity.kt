decad4b54db565f7282df8ae393ecb60a7375c81
Joe Scharnitzke <j.scharnitzke@gmail.com>
2023-12-30 14:23:15 -0500

    center signature text horizontally

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -64,7 +64,7 @@ fun GreetingText(from: String, message: String, modifier: Modifier = Modifier) {
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End),
+                .align(alignment = Alignment.CenterHorizontally),
             text = stringResource(R.string.signature_text, from)
         )
     }
